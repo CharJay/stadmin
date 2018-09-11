793a105521480f7bed4de92a851388e82072d473
charjay <820419196@qq.com>
2018-09-11 09:53:41 +0800

    init_data.sql

--- stadmin-mvc/src/main/java/com/xm/stadmin/App.java
+++ stadmin-mvc/src/main/java/com/xm/stadmin/App.java
@@ -1,8 +1,13 @@
 package com.xm.stadmin;
 
 import com.framework.core.db.dynamic.DynamicDataSourceRegister;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
@@ -24,11 +29,31 @@ public class App {
 	}
 	//u-insert#other3#
 	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+		tomcat.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+
+			@Override
+			public void customize(Connector arg0) {
+				Http11NioProtocol handler = (Http11NioProtocol) arg0.getProtocolHandler();
+				// 设置最大连接数
+				handler.setMaxConnections(2000);
+				// 设置最大线程数
+				handler.setMaxThreads(2000);
+				handler.setConnectionTimeout(30000);
+
+			}
+		});
+		return tomcat;
+	}
+
+	@Bean
 	public ThreadPoolTaskExecutor taskExecutor() {
 		ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
-		pool.setCorePoolSize(1);
+		pool.setCorePoolSize(5);
 		pool.setMaxPoolSize(100);
 		pool.setWaitForTasksToCompleteOnShutdown(true);
+
 		return pool;
 	}
 	/**
@@ -38,9 +63,10 @@ public class App {
 	@Bean
 	public TaskScheduler taskScheduler() {
 		ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-		scheduler.setPoolSize(3);//可根据并发定时任务数设置
+		scheduler.setPoolSize(4);//可根据并发定时任务数设置
 		return scheduler;
 	}
+
 	//u-insert#
 	
 }
